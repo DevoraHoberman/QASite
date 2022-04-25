10e37ec933f5c42004cad9735ed9b665d7ae9fbb
Devora Hoberman <devorahoberman1gmail.com>
2022-04-24 23:55:03 -0400

    Add project files.

---
+++ QASite.Data/Answer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QASite.Data
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public DateTime Date { get; set; }
+        public User User { get; set; }
+        public int UserID { get; set; }
+    }
+}
---
+++ QASite.Data/Migrations/20220424172101_Added stuff.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QASite.Data.Migrations
+{
+    public partial class Addedstuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tag",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Tag",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
